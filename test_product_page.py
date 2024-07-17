5107e8045f80f8ef6258ccff54d4011f661be8ba
albina6 <m_ab3@mail.ru>
2024-07-17 13:51:16 +0500

    Add positiv test -add product in cart, add product page go to login(and should see)

--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,7 @@ import time
 import pytest
 from .pages.locators import ProductPageLocators
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 
 
 # link_list ='''\
@@ -19,37 +20,57 @@ from .pages.product_page import ProductPage
 
 # @pytest.mark.parametrize('link', link_list)
 @pytest.mark.xfail(reason="Red mark test")
-def test_add_product_from_product_page(browser):#, link):
+def test_add_product_from_product_page_with_promo(browser):#, link):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    # link ="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
-    page.add_product()
+    page.add_product_with_promo()
     page.shold_is_disappeared_success_message()
-    # time.sleep(3)
+
+
+def test_add_product_from_product_page_without_promo(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+    page.add_product_simple()    
+
 
 @pytest.mark.xfail(reason="Red mark test")
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
-    page.add_product()
+    page.add_product_with_promo()
     assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE_LINK), "Show success message after adding product"
-    
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(page.browser, page.url)
+    login_page.should_be_login_page()
+
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
     assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE_LINK), "Show success message before adding product"
 
-@pytest.mark.xfail(reason="Red mark test")
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
-    page.add_product()
+    page.should_be_login_link()
+
+@pytest.mark.xfail(reason="Red mark test")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_simple()
     assert page.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE_LINK), "Success message is not disappeared after adding product"
 
 
